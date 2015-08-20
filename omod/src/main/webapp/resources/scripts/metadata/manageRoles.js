354951eb1baccf74e00d8b0aa457ba19a0addc6c
jdegraft <jdegraft_johnson@msn.com>
2015-08-19 19:50:03 -0500

    RA-806 Manage Roles Completed Manage Roles pages

---
+++ omod/src/main/webapp/resources/scripts/metadata/manageRoles.js
@@ -0,0 +1,283 @@
+angular.module("manageRoles", [ "roleService", "privilegeService", "ngDialog", "ui.router", "uicommons.filters", "colTable" ])
+
+    .config([ "$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/list");
+
+        $stateProvider
+            .state('list', {
+                url: "/list",
+                templateUrl: "templates/list.page",
+                controller: "ManageRolesController"
+            })
+            .state("edit", {
+                url: "/edit/:roleUuid",
+                templateUrl: "templates/edit.page",
+                params: {
+                    roleUuid: null,
+                },
+                resolve: {
+                    role: function($stateParams, Role) {
+                        if ($stateParams.roleUuid) {
+                            return Role.get({ uuid: $stateParams.roleUuid, v: "full", includeAll: true });
+                        }
+                        return {};
+                    }
+                },
+                controller: "EditRoleController"
+            });
+    }])
+
+    .controller("ManageRolesController", [ "$scope", "$state", "Role", "ngDialog", 
+        function($scope, $state, Role, ngDialog) {
+            function sortWithRetiredLast(list) {
+                return _.sortBy(list, "retired");
+            }
+
+            function loadRoles() {
+                // TODO standard function for failure of REST call
+                //Role.query({ v: "default", includeAll: true }).$promise.then(function(response) {
+                Role.query({ v: "full", includeAll: true }).$promise.then(function(response) {
+                    // TODO handle multiple pages of results in a standard way
+                    $scope.roles = sortWithRetiredLast(response.results);
+                });
+            }
+
+            $scope.retire = function(role) {
+                // TODO manage roles retire
+                ngDialog.openConfirm({
+                    showClose: false,
+                    closeByEscape: true,
+                    closeByDocument: true,
+                    template: "templates/retireRoleDialog.page",
+                    controller: function($scope) {
+                        $scope.role = role;
+                    }
+                }).then(function(role, reason) {
+                    var toSave = {
+                        uuid: role.uuid,
+                        //name: $scope.role.name,
+                        //description: $scope.role.description,
+                        //inheritedRoles: $scope.role.inheritedRoles,
+                        //privileges: $scope.role.privileges,
+                        retired: true,
+                        reason: reason
+                    }
+                    // will fail until RESTWS-456
+                    Role.save(
+                        toSave
+                    ).$promise.then(function() {
+                        // loadRoles();
+                        $state.reload(); 
+                    });
+                });
+            }
+
+            $scope.unretire = function(role) {
+                // TODO manage roles unretire
+                var toSave = {
+                    uuid: role.uuid,
+                    //name: $scope.role.name,
+                    //description: $scope.role.description,
+                    //inheritedRoles: $scope.role.inheritedRoles,
+                    //privileges: $scope.role.privileges,
+                    retired: false
+                }
+                // will fail until RESTWS-456
+                Role.save(
+                    toSave
+                ).$promise.then(function() {
+                    // loadRoles();
+                    $state.reload(); 
+                })
+            }
+
+            $scope.edit = function(role) {
+                $state.go("edit", { roleUuid: role.uuid });
+            }
+
+            $scope.purge = function(role) {
+                ngDialog.openConfirm({
+                    showClose: false,
+                    closeByEscape: true,
+                    closeByDocument: true,
+                    template: "templates/purgeRoleDialog.page",
+                    controller: function($scope) {
+                        $scope.role = role;
+                    }
+                }).then(function() {
+                    Role.delete({
+                        uuid: role.uuid,
+                        purge: ""
+                    }).$promise.then(function() {
+                        //loadRoles();
+                        emr.successMessage(emr.message("adminui.role.purge.success"));
+                        $state.reload(); 
+                     })
+                });
+            }
+
+            loadRoles();
+        }])
+
+    .controller("EditRoleController", [ "$scope", "$state", "Role", "role", "Privilege",
+        function($scope, $state, Role, role, Privilege) {
+            $scope.role = role;
+
+            function sortWithRetiredLast(list) {
+                return _.sortBy(list, "retired");
+            }
+            
+            // test whether an object is contained within an array
+            function isInArray(myArray, myValue)
+            {
+            	var inArray = false;
+                if (myArray != null) {
+                    myArray.map(function(arrayObj){
+                        if (arrayObj.uuid === myValue.uuid) {
+                            inArray = true;
+                        }
+                    });
+                }
+                return inArray;
+            }
+            
+            // get name of privilege
+            function getNamePrivilege(myArray, myValue)
+            {
+            	var name = myValue.uuid;
+                if (myArray != null) {
+                    myArray.map(function(arrayObj){
+                        if (arrayObj.uuid === myValue.uuid) {
+                            name = arrayObj.name;
+                        }
+                    });
+                }
+                return name;
+            }
+            
+            // load inherited privileges
+            function loadInheritedPrivileges() {
+                $scope.inheritedPrivileges = [];         
+                       
+                if ($scope.role.allInheritedRoles != null) {
+                    
+                    $scope.role.inheritedRoles.forEach(function(val, idx) {
+                        if (val.privileges != null) {
+                            
+                            val.privileges.forEach(function(inp, inpx){
+                                if (!isInArray($scope.inheritedPrivileges, inp)) { // no duplicates
+                                    var inpPrivilege = {
+                                        uuid: inp.uuid,
+                                        name: getNamePrivilege($scope.privileges, inp)
+                                    }
+                                    $scope.inheritedPrivileges.push(inpPrivilege);
+                                }
+                            });
+                        }
+                    });
+                }                
+           }
+            
+            // load roles with indication of those contained in current role inherited roles
+            function loadRoles() {
+                // TODO standard function for failure of REST call
+                Role.query({ v: "full", includeAll: true }).$promise.then(function(response) {
+                    // TODO handle multiple pages of results in a standard way
+                    $scope.roles = sortWithRetiredLast(response.results);
+                    
+                    //remove self from array
+                    var idx = $scope.roles.length;
+                    while(idx--){
+                        if ($scope.roles[idx].uuid === role.uuid)  { 
+                            break;
+                        }
+                    }
+                    $scope.roles.splice(idx, 1);  
+                                      
+                    $scope.inheritedRoles = Array($scope.roles.length);
+                    if ($scope.roles != null) {
+                        
+                        $scope.dependantRoles = []; 
+                         
+                        $scope.roles.forEach(function(val, idx) { 
+                            
+                            $scope.inheritedRoles[idx] = isInArray($scope.role.inheritedRoles, val);
+                            
+                            // load dependant roles
+                            if (!isInArray( $scope.dependantRoles, val)) { // no duplicates
+                                if (val.allInheritedRoles != null) {
+                                    if (isInArray(val.allInheritedRoles, $scope.role)) {
+                                        var depRole = {
+                                            uuid: val.uuid,
+                                            name: val.name
+                                        }
+                                        $scope.dependantRoles.push(depRole);                        
+                                    }
+                                }
+                            }
+                        });
+                    }
+               });               
+               loadPrivileges();
+               loadInheritedPrivileges();
+            }
+
+              // load privileges with indication of those contained in current role      
+            function loadPrivileges() {                
+                // TODO standard function for failure of REST call
+                Privilege.query({ v: "full", includeAll: true }).$promise.then(function(response) {
+                    // TODO handle multiple pages of results in a standard way
+                    $scope.privileges = sortWithRetiredLast(response.results);                    
+                    $scope.privilegeFlags = Array($scope.privileges.length);
+                    if ($scope.privileges != null) {
+                        $scope.privileges.forEach(function(val, idx) { 
+                            $scope.privilegeFlags[idx] = isInArray($scope.role.privileges, val);                            
+                        });
+                    }
+                });
+            }
+
+            $scope.save = function() {
+                // save selected inherited roles
+                $scope.role.inheritedRoles = []; // clear list
+                var idx = $scope.inheritedRoles.length;
+                while(idx--){
+                    if ($scope.inheritedRoles[idx])  { // role selected
+                        var inheritedRole = {}; 
+                        
+                        inheritedRole.uuid = $scope.roles[idx].uuid;
+                         
+                        $scope.role.inheritedRoles.push(inheritedRole);  // add role to list 
+                    }
+                }               
+                // save selected privilegess
+                $scope.role.privileges = []; 
+                idx = $scope.privilegeFlags.length;
+                while(idx--){
+                    if ($scope.privilegeFlags[idx])  { 
+                        var privilege = {}; 
+                        
+                        privilege.uuid = $scope.privileges[idx].uuid;
+                         
+                        $scope.role.privileges.push(privilege);   
+                    }
+                }
+
+               // TODO: utility function for doing this, until RESTWS-460 is fixed
+                var toSave = {
+                    uuid: $scope.role.uuid,
+                    name: $scope.role.name,
+                    description: $scope.role.description,
+                    inheritedRoles: $scope.role.inheritedRoles,
+                    privileges: $scope.role.privileges
+                }
+                Role.save(toSave).$promise.then(function() {
+                    $state.go("list");
+                    emr.successMessage(emr.message("uicommons.generalSavedNotification"));
+                }, function() {
+                    // TODO handle server-side errors
+                })
+            }
+
+            loadRoles();
+    }]);
